1384a128b257389888ad7f3d94804aa160302b57
gabrieldemian <gabrielgcr45@gmail.com>
2024-08-19 14:38:42 +0300

    new fn extract_inner_type

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,57 @@
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{quote, ToTokens};
 use syn::{
-    parse_macro_input, AngleBracketedGenericArguments, Data, DataStruct,
-    DeriveInput, Fields, FieldsNamed, GenericArgument, Ident, Path,
-    PathArguments, Type, TypePath,
+    parse::{Parse, ParseStream, Parser},
+    parse_macro_input, AngleBracketedGenericArguments, Attribute, Data,
+    DataStruct, DeriveInput, Expr, ExprLit, Fields, FieldsNamed,
+    GenericArgument, Ident, Lit, MetaNameValue, PathArguments, Result, Token,
+    Type, TypePath,
 };
+
+/// Try to extract the inner type of a type:
+/// example: Option<T> or Vec<T>
+///
+/// Return: (inner, outter)
+/// (T, Option)
+///
+/// ```ignore
+/// let t: Ident = syn::parse_str("std::vec::Vec<String>")?;
+/// extract_inner_type(&myty, &t)
+/// ```
+fn extract_inner_type<'a>(
+    ty: &'a syn::Type,
+    inner: &'a syn::Ident,
+) -> Option<(syn::Ident, syn::Type)> {
+    let syn::Type::Path(TypePath { qself: None, ref path }) = ty else {
+        return None;
+    };
+
+    let segment = path.segments.last()?;
+
+    let PathArguments::AngleBracketed(AngleBracketedGenericArguments {
+        ref args,
+        ..
+    }) = segment.arguments
+    else {
+        return None;
+    };
+    let GenericArgument::Type(Type::Path(TypePath { qself: None, path })) =
+        args.last()?
+    else {
+        return None;
+    };
+
+    let inner_found = &path.segments.last()?.ident;
+
+    if inner == inner_found {
+        Some((
+            inner.clone(),
+            Type::Path(TypePath { qself: None, path: path.clone() }),
+        ))
+    } else {
+        None
+    }
+}
 
 fn extract_type_from_option(ty: &syn::Type) -> Option<&syn::Type> {
     use syn::{GenericArgument, Path, PathArguments, PathSegment};
@@ -49,6 +96,23 @@ fn extract_type_from_option(ty: &syn::Type) -> Option<&syn::Type> {
             GenericArgument::Type(ref ty) => Some(ty),
             _ => None,
         })
+}
+
+struct StructField {
+    // only accept #[builder(arg = "x")]
+    attrs: Vec<Attribute>,
+    name: Ident,
+    ty: Type,
+}
+
+impl Parse for StructField {
+    fn parse(input: ParseStream) -> Result<Self> {
+        Ok(StructField {
+            attrs: input.call(Attribute::parse_outer)?,
+            name: input.parse()?,
+            ty: input.parse()?,
+        })
+    }
 }
 
 #[proc_macro_derive(Builder, attributes(builder))]
@@ -97,19 +161,69 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let mut method = Vec::new();
 
     for field in struct_field {
+        let attr = &field.attrs;
+
         let ident = field.ident.as_ref().unwrap();
         let mut ty = &field.ty;
 
+        let is_vec = if let Type::Path(TypePath {
+            qself: None,
+            path: syn::Path { segments, .. },
+        }) = &ty
+        {
+            let q = &segments[0].ident;
+            eprintln!("segments {:#?}", &segments);
+            q == "Vec"
+        } else {
+            false
+        };
+
+        if !attr.is_empty() {
+            let att = &attr[0];
+            let parsed: MetaNameValue = att.parse_args().unwrap();
+            let key = &parsed.path.segments[0].ident;
+            let Expr::Lit(ExprLit { lit: Lit::Str(value), .. }) = &parsed.value
+            else {
+                panic!("Wrong attribute syntax for builder");
+            };
+
+            // remove quotes from the string
+            let value: Ident = value.parse().unwrap();
+
+            if !is_vec {
+                panic!("Wrong syntax for builder, can only use 'each' for a Vec type");
+            };
+
+            if key == "each" {
+                let topush = quote! {
+                    pub fn #value(&mut self, v: String) -> &mut Self {
+                        self.#ident.push(v);
+                        self
+                    }
+                };
+                // eprintln!("topush {}", topush);
+                method.push(topush);
+            }
+        }
+
         let opt = extract_type_from_option(ty);
         let is_option = opt.is_some();
+
+        eprintln!("is_option {is_option} is_vec {is_vec}");
 
         if let Some(opt) = opt {
             ty = opt;
         }
 
-        struct_name_type.push(quote! {
-            #ident: Option<#ty>,
-        });
+        if is_vec {
+            struct_name_type.push(quote! {
+                #ident: #ty,
+            });
+        } else {
+            struct_name_type.push(quote! {
+                #ident: Option<#ty>,
+            });
+        }
 
         if is_option {
             struct_ident_opt.push(ident);
@@ -121,12 +235,23 @@ pub fn derive(input: TokenStream) -> TokenStream {
             #ident: None,
         });
 
-        method.push(quote! {
-            pub fn #ident(&mut self, v: #ty) -> &mut Self {
-                self.#ident = Some(v);
-                self
+        let topush = if is_vec {
+            quote! {
+                pub fn #ident(&mut self, v: #ty) -> &mut Self {
+                    self.#ident.extend_from_slice(&v);
+                    self
+                }
             }
-        });
+        } else {
+            quote! {
+                pub fn #ident(&mut self, v: #ty) -> &mut Self {
+                    self.#ident = Some(v);
+                    self
+                }
+            }
+        };
+
+        method.push(topush);
     }
 
     let expr = quote! {
--- builder/tests/progress.rs
+++ builder/tests/progress.rs
@@ -2,30 +2,34 @@ use derive_builder::Builder;
 
 #[test]
 fn tests() {
-    // #[derive(Builder)]
-    // pub struct Command {
-    //     executable: String,
-    //     // #[builder(each = "arg")]
-    //     // args: Vec<String>,
-    //     // #[builder(each = "env")]
-    //     env: Vec<String>,
-    //     current_dir: Option<String>,
-    // }
-    // let command = Command::builder()
-    //     .executable("cargo".to_owned())
-    //     .args(vec!["build".to_owned(), "--release".to_owned()])
-    //     .env(vec![])
-    //     .build()
-    //     .unwrap();
+    let mut v: Vec<u32> = Vec::new();
+    let x = vec![2];
+    v.extend_from_slice(&x);
+
+    #[derive(Builder)]
+    pub struct Command {
+        executable: String,
+        #[builder(each = "arg")]
+        args: Vec<String>,
+        // #[builder(each = "env")]
+        // env: Vec<String>,
+        current_dir: Option<String>,
+    }
+    let command = Command::builder()
+        .executable("cargo".to_owned())
+        // .args(vec!["build".to_owned(), "--release".to_owned()])
+        // .env(vec![])
+        .build()
+        .unwrap();
 
     let t = trybuild::TestCases::new();
-    t.pass("tests/01-parse.rs");
-    t.pass("tests/02-create-builder.rs");
-    t.pass("tests/03-call-setters.rs");
-    t.pass("tests/04-call-build.rs");
-    t.pass("tests/05-method-chaining.rs");
+    // t.pass("tests/01-parse.rs");
+    // t.pass("tests/02-create-builder.rs");
+    // t.pass("tests/03-call-setters.rs");
+    // t.pass("tests/04-call-build.rs");
+    // t.pass("tests/05-method-chaining.rs");
     t.pass("tests/06-optional-field.rs");
-    //t.pass("tests/07-repeated-field.rs");
+    // t.pass("tests/07-repeated-field.rs");
     //t.compile_fail("tests/08-unrecognized-attribute.rs");
     //t.pass("tests/09-redefined-prelude-types.rs");
 }
